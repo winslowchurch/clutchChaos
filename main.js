b431ceb44ce0076d9ad32382d94a8d2f8ec03ef2
Winslow Church <winslowc@stanford.edu>
2025-01-17 09:44:26 -0800

    basics

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// Create a new Phaser game
 const config = {
-    type: Phaser.AUTO, // Automatically choose between WebGL and Canvas
-    width: 800, 
+    type: Phaser.AUTO,
+    width: 800,
     height: 600,
     scene: {
         preload: preload,
@@ -12,14 +11,42 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let player;
+
 function preload() {
     this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
+    this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
 }
 
 function create() {
     this.add.image(400, 300, 'sky');
+
+    // Check if player is loaded
+    console.log('Loading player sprite...');
+    player = this.physics.add.image(400, 300, 'player');
+    player.setCollideWorldBounds(true); // Keep player in bounds
+    
+    // Check if player was created
+    console.log('Hi', player);
 }
 
 function update() {
-    
+    // Player movement
+    const cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown) {
+        player.setVelocityY(-160);
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(160);
+    } else {
+        player.setVelocityY(0);
+    }
 }

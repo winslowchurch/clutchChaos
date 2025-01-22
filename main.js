e14ffc4d14165eb4a22bc585075eb98b746569d3
Winslow Church <winslowc@stanford.edu>
2025-01-21 21:43:53 -0800

    center screen

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ import { createGirl } from "./girl.js";
 // mugger sprite 
 // mugger cursing sounds
 
+// LATER
+// Levels
+// Add failure possibility
+
 const config = {
     type: Phaser.AUTO,
     width: 1000,
@@ -42,7 +46,6 @@ function preload() {
     this.load.image('girl1', 'images/girl1.png'); 
     this.load.image('girl2', 'images/girl2.png');
     this.load.image('happyGirl', 'images/happyGirl.png');
-
 
     // Sounds
     this.load.audio('lipstickSound', 'assets/whoosh1.mp3');
